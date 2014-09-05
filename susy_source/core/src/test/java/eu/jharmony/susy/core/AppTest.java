f8d6aa165ac28fbfd46835efb4d8e6fdf194ce3c
Mechitar <mmc.kaktus@wp.pl>
2014-09-05 14:22:19 +0200

    initial impoer of the susy-core project

---
+++ susy_source/core/src/test/java/eu/jharmony/susy/core/AppTest.java
@@ -0,0 +1,20 @@
+package eu.jharmony.susy.core;
+
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+    }
+}
